0a7111352ef4f60ac318d04256fc9efc33116c9a
joao vitor <resetgamer@gmail.com>
2019-12-30 03:22:45 -0300

    jogo finalizado

---
+++ 1st-release/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import http from 'http';
+import createGame from './public/game.js';
+import socketio from 'socket.io';
+import { connect } from 'http2';
+
+const app = express();
+const server = http.createServer(app);
+const sockets = socketio(server);
+
+app.use(express.static('public'));
+
+const game = createGame();
+game.start();
+
+game.subscribe((command) => {
+    console.log(`> Emitting ${command.type}`);
+    sockets.emit(command.type, command);
+})
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id;
+    console.log(`> Player connected ${playerId}`); 
+
+    game.addPlayer({ playerId: playerId,  });
+    socket.emit('setup', game.state);
+
+    socket.on('disconnect', () => {
+        game.removePlayer({ playerId: playerId });
+        console.log(`> Player left: ${playerId}`);  
+    })
+
+    socket.on('move-player', (command) => {
+        command.playerId = playerId;
+        command.type = 'move-player'
+
+        game.movePlayer(command);
+    })
+})
+
+server.listen(3333, () => {
+    console.log('Sever is running on port: http://localhost:3333');
+});
